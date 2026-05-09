3633c25347b69995ea96afdaae2f053ad3d4d0a0
Aamir <darksabre48@gmail.com>
2026-05-09 10:45:26 +0200

    Add ship movement with tests

---
+++ Assets/_Asteroids/Scripts/Ship/ShipMovement.cs
@@ -0,0 +1,54 @@
+using Asteroids.Input;
+using UnityEngine;
+using VContainer;
+
+namespace Asteroids.Ship {
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class ShipMovement : MonoBehaviour {
+        [SerializeField] private float _thrustForce = 5f;
+        [SerializeField] private float _rotationSpeed = 200f;
+
+        private Rigidbody2D _rigidbody2D;
+        private IInputReader _inputReader;
+        private bool _isThrusting;
+        private float _rotationInput;
+
+        [Inject]
+        public void Construct(IInputReader inputReader) {
+            _inputReader = inputReader;
+        }
+
+        private void Awake() {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        private void OnEnable() {
+            _inputReader.ThrustInput += OnThrustInput;
+            _inputReader.RotateInput += OnRotateInput;
+        }
+
+        private void OnDisable() {
+            _inputReader.ThrustInput -= OnThrustInput;
+            _inputReader.RotateInput -= OnRotateInput;
+        }
+
+        private void FixedUpdate() {
+            if (_isThrusting) {
+                _rigidbody2D.AddForce(transform.up * _thrustForce);
+            }
+
+            if (_rotationInput != 0) {
+                _rigidbody2D.MoveRotation(_rigidbody2D.rotation -
+                                          _rotationInput * _rotationSpeed * Time.fixedDeltaTime);
+            }
+        }
+
+        private void OnThrustInput(bool isThrusting) {
+            _isThrusting = isThrusting;
+        }
+
+        private void OnRotateInput(float rotation) {
+            _rotationInput = rotation;
+        }
+    }
+}
---
+++ Assets/_Asteroids/Tests/PlayMode/ShipMovementTests.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using Asteroids.Input;
+using Asteroids.Ship;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Object = UnityEngine.Object;
+
+namespace Asteroids.Tests.PlayMode {
+    public class ShipMovementTests {
+        private GameObject _ship;
+        private ShipMovement _shipMovement;
+        private IInputReader _inputReader;
+
+        [SetUp]
+        public void Setup() {
+            _ship = new GameObject();
+            _ship.SetActive(false);
+            _ship.AddComponent<Rigidbody2D>().gravityScale = 0;
+            _shipMovement = _ship.AddComponent<ShipMovement>();
+            _inputReader = Substitute.For<IInputReader>();
+            _shipMovement.Construct(_inputReader);
+            _ship.SetActive(true);
+        }
+
+        [TearDown]
+        public void TearDown() {
+            Object.Destroy(_ship);
+        }
+
+        [UnityTest]
+        public IEnumerator ShipMovement_WhenThrusting_MovesForward() {
+            Vector3 initialPosition = _ship.transform.position;
+            _inputReader.ThrustInput += Raise.Event<Action<bool>>(true);
+            yield return new WaitForFixedUpdate();
+            Assert.That(_ship.transform.position.y, Is.GreaterThan(initialPosition.y));
+        }
+
+        [UnityTest]
+        public IEnumerator ShipMovement_WhenNotThrusting_DoesNotMove() {
+            Vector3 initialPosition = _ship.transform.position;
+            _inputReader.ThrustInput += Raise.Event<Action<bool>>(false);
+            yield return new WaitForFixedUpdate();
+            Assert.AreEqual(initialPosition, _ship.transform.position);
+        }
+
+        [UnityTest]
+        public IEnumerator ShipMovement_WhenRotatingRight_RotatesClockwise() {
+            Rigidbody2D rb = _ship.GetComponent<Rigidbody2D>();
+            float initialRotation = rb.rotation;
+            _inputReader.RotateInput += Raise.Event<Action<float>>(1f);
+            yield return new WaitForFixedUpdate();
+            Assert.That(rb.rotation, Is.LessThan(initialRotation));
+        }
+
+        [UnityTest]
+        public IEnumerator ShipMovement_WhenRotatingLeft_RotatesCounterClockwise() {
+            Rigidbody2D rb = _ship.GetComponent<Rigidbody2D>();
+            float initialRotation = rb.rotation;
+            _inputReader.RotateInput += Raise.Event<Action<float>>(-1f);
+            yield return new WaitForFixedUpdate();
+            Assert.That(rb.rotation, Is.GreaterThan(initialRotation));
+        }
+    }
+}
