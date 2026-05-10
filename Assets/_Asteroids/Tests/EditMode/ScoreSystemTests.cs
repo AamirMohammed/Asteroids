005cd3afb1d741bacc5548b5cf03cb2e3ef4108d
Aamir <darksabre48@gmail.com>
2026-05-10 03:02:25 +0200

    Add ScoreSystem tests

---
+++ Assets/_Asteroids/Tests/EditMode/ScoreSystemTests.cs
@@ -0,0 +1,49 @@
+using Asteroids.Scoring;
+using NUnit.Framework;
+
+namespace Asteroids.Tests.EditMode {
+    public class ScoreSystemTests {
+        private ScoreSystem _scoreSystem;
+
+        [SetUp]
+        public void Setup() {
+            _scoreSystem = new ScoreSystem();
+        }
+
+        [Test]
+        public void AddScore_WhenCalled_IncreasesScore() {
+            _scoreSystem.AddScore(20);
+            Assert.That(_scoreSystem.Score, Is.EqualTo(20));
+        }
+
+        [Test]
+        public void AddScore_MultipleTimes_AccumulatesScore() {
+            _scoreSystem.AddScore(20);
+            _scoreSystem.AddScore(50);
+            Assert.That(_scoreSystem.Score, Is.EqualTo(70));
+        }
+
+        [Test]
+        public void AddScore_WhenCalled_FiresScoreChangedEvent() {
+            int receivedScore = 0;
+            _scoreSystem.ScoreChanged += score => { receivedScore = score; };
+            _scoreSystem.AddScore(20);
+            Assert.That(receivedScore, Is.EqualTo(20));
+        }
+
+
+        [Test]
+        public void AddScore_MultipleTimes_EventFiresWithAccumulatedScore() {
+            int lastReceivedScore = 0;
+            _scoreSystem.ScoreChanged += score => { lastReceivedScore = score; };
+            _scoreSystem.AddScore(20);
+            _scoreSystem.AddScore(50);
+            Assert.That(lastReceivedScore, Is.EqualTo(70));
+        }
+
+        [Test]
+        public void Score_InitialValue_IsZero() {
+            Assert.That(_scoreSystem.Score, Is.EqualTo(0));
+        }
+    }
+}
